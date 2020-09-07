d448d18380e8c3fcf5a4da329c0eb810753ebeca
Narayan <nspatilme@gmail.com>
2020-09-07 17:59:54 +0530

    Good BYE

--- blog/models.py
+++ blog/models.py
@@ -27,4 +27,4 @@ class Comment(models.Model):
     date_posted=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.Question)+str(self.Your_name)
+        return self.Question,self.Your_name
