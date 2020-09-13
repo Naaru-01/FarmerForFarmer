c2452ca2bbe2e8753d015f644282c6fec4d10a68
Ankush Patil <aspraz2658@gmail.com>
2020-09-14 01:16:48 +0530

    try1 deploy

--- blog/urls.py
+++ blog/urls.py
@@ -7,15 +7,15 @@ from .views import (
     PostDeleteView,
     UserPostListView,
     AddCommentView,
-   
-    
+
+
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-experience'),
     path('answers/', AddCommentView.as_view(), name='add-comment'),
-    
+
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('home/', views.about, name='blog-home1'),
 
-    
-    
+
+
 
 ]
