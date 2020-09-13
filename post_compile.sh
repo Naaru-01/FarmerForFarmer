9f5fae70d2356faf38b0cae763ad43e7bdf1a830
Ankush Patil <aspraz2658@gmail.com>
2020-09-14 01:32:10 +0530

    try1 deploy

--- post_compile.sh
+++ post_compile.sh
@@ -3,5 +3,5 @@ python manage.py clean_pyc
 python manage.py reset_schema
 python manage.py reset_db
 
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations blog
+python manage.py migrate blog
